fbba4849b85f71735350d0c962c33460315bd08b
Timothee <timothee.callet@combodo.com>
2023-06-14 14:45:44 +0200

    Dictionaries update for 3.1 beta

--- fr.dict.combodo-password-expiration.php
+++ fr.dict.combodo-password-expiration.php
@@ -5,7 +5,6 @@
  * @copyright   Copyright (C) 2021 Combodo SARL
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
-
 Dict::Add('FR FR', 'French', 'Français', array(
 	// Dictionary entries go here
 	'Class:UserLocal/Attribute:expiration+' => 'Réglages de validité des mots de passe',
