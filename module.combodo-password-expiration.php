a84ca2e23b51f4ed450aba187cc2a57902ec3dc1
Timothee <timothee.callet@combodo.com>
2023-06-15 12:20:40 +0200

    Suffix module version with -dev for beta release

--- module.combodo-password-expiration.php
+++ module.combodo-password-expiration.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-password-expiration/1.0.0',
+	'combodo-password-expiration/1.0.1-dev',
 	array(
 		// Identification
 		//
